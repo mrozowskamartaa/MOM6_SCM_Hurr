337cfa1e3a420929c54cdb4ef4baa9952a05d63c
Marta Mrozowska <mmrozowska7@gmail.com>
2026-05-13 12:36:52 -0400

    updating functions to make training dataset: separate utility file for all physics, math and array handling; extra method for running GOTM experiments (on temporal grid scaled by f and vertical grid scaled by Ekman depth; added methods to extract N2 and S2 profiles)

--- other_scripts/generate_gotm_training_sets/gotm_runner.py
+++ other_scripts/generate_gotm_training_sets/gotm_runner.py
@@ -1,83 +1,16 @@
 import os
 import shutil
-import re
-from datetime import datetime, timedelta
 
-import numpy as np
-
-
-def generate_time_range(start, end, delta):
-    current_date = start
-    while current_date < end:
-        yield current_date
-        current_date += timedelta(days=delta)
-
-
-# IMPORTANT: ASSUMES 1m VERTICAL SPACING
-def make_t_profile(
-        case_dir: str,
-        depth: int,
-        mld: int,
-        temp_grad: float = 0.04,
-        mld_temp: float = 29.25
-) -> str:
-    
-    t_profile = np.empty(depth)
-    t_profile[0:mld] = mld_temp
-    t_profile[mld:] = mld_temp - temp_grad*np.arange(1,depth-mld+1,1)
-
-    z_start = - depth + 0.5
-    z_end = 0.5
-    z_levels = np.arange(z_start,z_end,1)[::-1]
-
-    date_start = datetime(2011, 4, 1)
-    date_end = datetime(2012, 5, 1)
-    delta = 1
-    date_list = list(generate_time_range(date_start, date_end, delta))
-
-    filename = os.path.join(case_dir, "t_profile.dat")
-
-    with open(filename, "w", encoding='utf-8') as file:
-        for date in date_list:
-            data_string_list = [f"{z}\t{t}" for z, t in zip(z_levels,t_profile)]
-            data_string = "\n".join(data_string_list)
-            file.write(f"{str(date)}\t{depth}\t{2}\n{data_string}\n")
-
-    return filename
-
-
-def edit_yaml(
-        file: str,
-        pattern: str,
-        new_string: str
-) -> None:
-    
-    with open(file, 'r') as f:
-        lines = f.readlines()
-
-    new_lines = []
-    line_to_edit = re.compile(pattern)
-
-    for line in lines:
-        match = line_to_edit.match(line)
-
-        if match:
-            indent = match.group('indent')
-            new_line = (f"{indent}{new_string}\n")
-            new_lines.append(new_line)
-            continue
-    
-        new_lines.append(line)
-
-    with open(file, 'w') as f:
-        f.writelines(new_lines)
+from utility import make_t_profile, edit_yaml
 
 
 def run_gotm_experiments(
         root_dir: str, 
         source_dir_name: str,
         training_set_name: str,
-        case_dict: dict
+        case_dict: dict,
+        depth: int = 400,
+        vertical_spacing: float = 1.0
     ) -> None:
 
     source_file = os.path.join(root_dir, source_dir_name, "gotm.yaml")
@@ -93,9 +26,10 @@ def run_gotm_experiments(
         t_profile_pattern = r'^(?P<indent>\s*)file:\s+t_prof.dat'
         t_profile_file = make_t_profile(
             case_dir=case_dir,
-            depth=400,
+            depth=depth,
             mld=1,
-            temp_grad=case_specs["temp_grad"]
+            temp_grad=case_specs["temp_grad"],
+            vertical_spacing=vertical_spacing
         )
 
         output_pattern = r'^(?P<indent>\s*)output_filename'
--- other_scripts/generate_gotm_training_sets/run.py
+++ other_scripts/generate_gotm_training_sets/run.py
@@ -1,28 +1,35 @@
 from gotm_runner import run_gotm_experiments
+# from gotm_runner_f_u_star_grid import run_gotm_experiments
 import json
 from itertools import product
 
 import numpy as np
 
 
-training_set_name = "ePBL_paper_validation_2283_corrected"
+training_set_name = "ePBL_paper_2283_corrected_dz0.1"
 gotm_case_dict = {}
 root_dir = "/gpfs/f5/gfdl_o/scratch/Marta.Mrozowska/hurricane_LES/make_smc_training_set"
 
-# temperature_gradients = [0.001, 0.01, 0.02, 0.04]
-temperature_gradients = [0.005, 0.015, 0.03]
-# temperature_gradients = [0.5, 1.]
-# wind_stresses = np.arange(0.1, 1.1, 0.1).tolist()
-wind_stresses = [0.25, 0.75, 0.95]
-# wind_stresses = [0.1, 0.5, 1.0]
-# wind_stresses = [0.1, 1.0]
-# latitudes = np.arange(10., 100., 10.).tolist()
-latitudes = [15., 25., 55., 85.]
-# latitudes = [10., 30., 60., 90.]
-# latitudes = [0.0, 1., 5., 60.]
-# heat_fluxes = np.arange(-100,125,25).tolist()
-heat_fluxes = [-90, -40, -10, 10, 40, 90]
-# heat_fluxes = [0.0, 100.0]
+temperature_gradients = [0.001, 0.01, 0.02, 0.04]  # ePBL paper, ePBL paper expanded
+# temperature_gradients = [0.001, 0.04]  # ePBL paper reduced
+# temperature_gradients = [0.005, 0.015, 0.03]  # ePBL paper validation, ePBL validation dz=0.1
+# temperature_gradients = [0.5, 1.]  # non-rotating
+# wind_stresses = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]  # ePBL paper expanded with smaller tx values
+wind_stresses = [0.01, 0.05, 0.1, 0.5, 1.0]  # ePBL paper expanded with smaller tx values (0.001 and 0.005 were too small)
+# wind_stresses = np.arange(0.1, 1.1, 0.1).tolist()  # ePBL paper expanded
+# wind_stresses = [0.25, 0.75, 0.95]  # ePBL paper validation
+# wind_stresses = [0.025, 0.25, 0.75]  # ePBL validation dz=0.1
+# wind_stresses = [0.1, 0.5, 1.0]  # ePBL paper
+# wind_stresses = [0.1, 1.0]  # non-rotating
+latitudes = np.arange(10., 100., 10.).tolist()  # ePBL paper expanded
+# latitudes = [15., 25., 55., 85.]  # ePBL paper validation, ePBL validation dz=0.1
+# latitudes = [10., 30., 60., 90.]  # ePBL paper
+# latitudes = [10., 90.]  # ePBL paper reduced
+# latitudes = [0.0, 1., 5., 60.]  # non-rotating
+# heat_fluxes = [0.0]
+heat_fluxes = np.arange(-100,125,25).tolist()  # ePBL paper expanded, ePBL paper
+# heat_fluxes = [-90, -40, -10, 10, 40, 90]  # ePBL paper validation
+# heat_fluxes = [0.0, 100.0]  # non-rotating
 
 for i, combo in enumerate(product(temperature_gradients, wind_stresses, latitudes, heat_fluxes)):
     temp_grad, tx, lat, hf = combo
